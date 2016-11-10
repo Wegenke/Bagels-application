917e91d3a249eb2403abb02b14da04becb2eeb8e
Matthew Wegenke <matthew.wegenke@gmail.com>
2016-11-10 11:28:36 -0600

    just some rearranging

--- bagels.js
+++ bagels.js
@@ -1,6 +1,16 @@
-var computerChoice1 =Math.floor(Math.random() * 10);
-var computerChoice2 =Math.floor(Math.random() * 10);
-var computerChoice3 =Math.floor(Math.random() * 10);
 
+var computerchoice1
+var computerChoice2
+var computerChoice3
+var computerNumbers = [computerChoice1, computerChoice2, computerChoice3];
 
-var answerBlock = [computerChoice1, computerChoice2, computerChoice3];
+var startGame = function(){
+  do{
+    computerChoice1 =Math.floor(Math.random() * 10);
+    computerChoice2 =Math.floor(Math.random() * 10);
+    computerChoice3 =Math.floor(Math.random() * 10);
+  }
+  while(computerChoice1 === computerChoice2 || computerChoice1 === computerChoice3 || computerChoice2 === computerChoice3)
+}
+startGame();
+alert(startGame);
