ad5009df0c2ef8883785c97a45b21e9a0b47a33e
Matthew Wegenke <matthew.wegenke@gmail.com>
2016-11-09 20:25:48 -0600

    Initial upload

---
+++ bagels.js
@@ -0,0 +1,6 @@
+var computerChoice1 =Math.floor(Math.random() * 10);
+var computerChoice2 =Math.floor(Math.random() * 10);
+var computerChoice3 =Math.floor(Math.random() * 10);
+
+
+var answerBlock = [computerChoice1, computerChoice2, computerChoice3];
