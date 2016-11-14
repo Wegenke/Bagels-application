db90e3c694f27101e4e8ae3a45cbba71f40ce41e
Matthew Wegenke <matthew.wegenke@gmail.com>
2016-11-14 11:35:12 -0600

    just another upload

--- bagels.js
+++ bagels.js
@@ -1,16 +1,58 @@
+var computerchoice1, computerChoice2, computerChoice3;
+var guessDigit1, guessDigit2, guessDigit3;
 
-var computerchoice1
-var computerChoice2
-var computerChoice3
-var computerNumbers = [computerChoice1, computerChoice2, computerChoice3];
+function getRandomNumber(){
+  return Math.floor(Math.random() * 10);
+};
 
-var startGame = function(){
+function compareUserVsComputer(){
+  if (computerChoice1 === guessDigit1){
+    return 'Pico';
+  } else if (computerChoice1 === guessDigit2 || computerChoice1 === guessDigit3){
+    return 'Fermi';
+  } else {
+    return 'null'
+  };
+
+  if (computerChoice2 === guessDigit2){
+    return 'Pico';
+  } esle if (computerChoice2 === guessDigit1 || computerChoice2 === guessDigit3){
+    return 'Fermi';
+  } else {
+    return 'null'
+  };
+
+  if (computerChoice3 === guessDigit3){
+    return 'Pico';
+  }else if (computerChoice3 === guessDigit2 || computerChoice1 === guessDigit1){
+    return 'Fermi';
+  }else {
+    return 'null'
+  };
+};
+
+function startGame(){
   do{
-    computerChoice1 =Math.floor(Math.random() * 10);
-    computerChoice2 =Math.floor(Math.random() * 10);
-    computerChoice3 =Math.floor(Math.random() * 10);
-  }
-  while(computerChoice1 === computerChoice2 || computerChoice1 === computerChoice3 || computerChoice2 === computerChoice3)
-}
+    computerChoice1 = getRandomNumber;
+    computerChoice2 = getRandomNumber;
+    computerChoice3 = getRandomNumber;
+  } while(computerChoice1 === computerChoice2 || computerChoice1 === computerChoice3 || computerChoice2 === computerChoice3)
+};
+
+function getUserGuess(){
+  guessDigit1 = document.getElementById("guess1").value;
+  guessDigit2 = document.getElementById("guess2").value;
+  guessDigit3 = document.getElementById("guess3").value;
+};
+
+document.getElementById('guessButton').addEventListener('click', function(){
+  getUserGuess();
+  getRandomNumber();
+});
+
+
+
+
 startGame();
-alert(startGame);
+var computerNumbers = [computerChoice1, computerChoice2, computerChoice3];
+alert(compareUserVsComputer);
