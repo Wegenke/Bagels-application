fbb30502ddd6ab8d117e32642a2c03b5d74ea392
Matthew Wegenke <matthew.wegenke@gmail.com>
2016-11-15 22:05:10 -0600

    2200 tuesday

--- bagels.js
+++ bagels.js
@@ -1,10 +1,9 @@
 function getRandomNumber(){
   return Math.floor(Math.random() * 10);
-};
+}
 
 var computerchoice1, computerChoice2, computerChoice3;
 var guessDigit1, guessDigit2, guessDigit3;
-var response = "";
 
 function startGame(){
   do{
@@ -18,7 +17,8 @@ function startGame(){
   || computerChoice2 == 0
   || computerChoice3 == 0
   );
-};
+  document.getElementById('guessButton').disabled = false;
+}
 
 document.getElementById('startGame').addEventListener('click', function(){
   startGame();
@@ -29,41 +29,27 @@ function getUserGuess(){
   guessDigit1 = document.getElementById("guess1").value;
   guessDigit2 = document.getElementById("guess2").value;
   guessDigit3 = document.getElementById("guess3").value;
-};
+}
 
 function compareUserVsComputer1(){
-   if (computerChoice1 == guessDigit1){
-    return response = "pico";
-  } else if (computerChoice1 == guessDigit2 || computerChoice1 == guessDigit3){
-    return response = 'fermi';
-  } else {
-    return response = "";
-  }
-};
+   if (computerChoice1 == guessDigit1) return "pico";
+    else if (computerChoice1 == guessDigit2 || computerChoice1 == guessDigit3)return  'fermi';
+    else return  "";
+}
 function compareUserVsComputer2(){
-  if (computerChoice2 == guessDigit2){
-    return response = "pico";
-  } else if (computerChoice2 == guessDigit1 || computerChoice2 == guessDigit3){
-    return response = 'fermi';
-  } else {
-    return response = "";
-  }
-};
+  if (computerChoice2 == guessDigit2) return  "pico";
+  else if (computerChoice2 == guessDigit1 || computerChoice2 == guessDigit3) return  'fermi';
+  else return  "";
+}
 function compareUserVsComputer3(){
-  if (computerChoice3 == guessDigit3){
-   return response = "pico";
-  }else if (computerChoice3 == guessDigit2 || computerChoice1 == guessDigit1){
-   return response = 'fermi';
-  } else {
-   return response = "";
-  }
-};
+  if (computerChoice3 == guessDigit3) return "pico";
+  else if (computerChoice3 == guessDigit2 || computerChoice1 == guessDigit1) return  'fermi';
+  else return "";
+}
 
 function previousGuess(){
-  var div = document.getElementById('guesses');
-  var nextLine = div.document.createElement("p")
-  var guessLine = "Guess 1: " + guessDigit1 + "     " + "Guess 2: " + guessDigit2 + "     " + "Guess 3: " + guessDigit3 + "     !!!" + "<br>";
-  nextLine.guessLine
+  var guessLine = "Guess 1: " + guessDigit1 + "     Guess 2: " + guessDigit2 + "     Guess 3: " + guessDigit3 + "!!!" + "<br>";
+  document.getElementById('guesses').innerHTML += guessLine;
 }
 
 document.getElementById('guessButton').addEventListener('click', function(){
@@ -79,7 +65,7 @@ document.getElementById('guessButton').addEventListener('click', function(){
   
 });
 
-var computerNumbers = [computerChoice1, computerChoice2, computerChoice3];
+// var computerNumbers = [computerChoice1, computerChoice2, computerChoice3];
 
 
 
